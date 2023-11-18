68562129bb04920c5adc0e7a9e5064f671fc3210
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-18 01:10:21 -0800

    dateFormat - add months back

--- server/utils/dateFormat.js
+++ server/utils/dateFormat.js
@@ -28,7 +28,15 @@ module.exports = (
   const months = {
     0: monthLength === 'short' ? 'Jan' : 'January',
     1: monthLength === 'short' ? 'Feb' : 'February',
-    // ... (remaining months)
+    2: monthLength === 'short' ? 'Mar' : 'March',
+    3: monthLength === 'short' ? 'Apr' : 'April',
+    4: monthLength === 'short' ? 'May' : 'May',
+    5: monthLength === 'short' ? 'Jun' : 'June',
+    6: monthLength === 'short' ? 'Jul' : 'July',
+    7: monthLength === 'short' ? 'Aug' : 'August',
+    8: monthLength === 'short' ? 'Sep' : 'September',
+    9: monthLength === 'short' ? 'Oct' : 'October',
+    10: monthLength === 'short' ? 'Nov' : 'November',
     11: monthLength === 'short' ? 'Dec' : 'December',
   };
 
