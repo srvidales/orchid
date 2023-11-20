9a8313d2bc9106d81c8e1855c868076d005921e2
charmingdarling <h1hasinai_f06@yahoo.com>
2023-11-19 23:40:11 -0800

    imported react-bootstrap, created data that can be mapped over to display as cards

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,43 @@
-const About = () => {
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+function About() {
+  const cardData = [
+    {
+      title: 'Our Daycare',
+      text: 'We are committed to upholding excellence and integrity in every facet of our daycare. Seeking inventive approaches to benefit our little learners is a priority. Collaboration and teamwork are essential components of our success. We extend dignity and respect to everyone we encounter. Our unwavering dedication is directed towards the well-being of families.',
+    },
+    {
+      title: 'Vision',
+      text: 'We take pride in empowering and celebrating each milestone of their development.',
+    },
+    {
+      title: 'Mission',
+      text: "Through our dedication in focusing on providing healthy and well-balanced meals, Wicked Whippersnappers ensures that your child's nutritional needs are met, nurturing their well-being.",
+    },
+    {
+      title: 'Values',
+      text: "Our learning approach and curriculum encourages exploration and progress tailored to each child's unique pace and style. We foster social, physical, emotional, and intellectual growth. Our team of highly trained educators is passionately committed to guiding, nurturing, and cultivating the full spectrum of your child's development.",
+    },
+  ];
+
   return (
-    <div>
-      <h1>About Us</h1>
-    </div>
+    <Row xs={1} md={2} className="g-3">
+      {cardData.map((card, idx) => (
+        <Col key={idx}>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>{card.title}</Card.Title>
+              <Card.Text>{card.text}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   );
-};
+}
 
 export default About;
