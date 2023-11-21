74133f742bf56857292d745a8a95cffc7e1ad9fa
Kim Nguyen <124004580+charmingdarling@users.noreply.github.com>
2023-11-20 23:41:34 -0800

    Packagejson (#26)
    * imported bootstrap css into App.jsx
    
    * npm i bootstrap and bootstrap-react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+// import './index.css';
 import { Outlet, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 
