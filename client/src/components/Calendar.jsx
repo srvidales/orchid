421f843e17a94daa9b5909440720c27664837524
Sergio Vidales Perez <srvidales@gmail.com>
2023-11-21 12:13:01 -0800

    WIP - Still testing implementation

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,147 +1,164 @@
+//<use xlink:href="#arrow-left-short"></use>
+//<use xlink:href="#arrow-right-short"></use>
+
 export default function Calendar() {
   return (
     <>
-      <div className="container text-center">
-        <div className="cal">
-          <div className="cal-month">
-            <button className="btn cal-btn" type="button">
-              <svg className="bi" width="16" height="16"></svg>
-            </button>
-            <strong className="cal-month-name">June</strong>
-            <select className="form-select cal-month-name d-none">
-              <option value="January">January</option>
-              <option value="February">February</option>
-              <option value="March">March</option>
-              <option value="April">April</option>
-              <option value="May">May</option>
-              <option selected="" value="June">
-                June
-              </option>
-              <option value="July">July</option>
-              <option value="August">August</option>
-              <option value="September">September</option>
-              <option value="October">October</option>
-              <option value="November">November</option>
-              <option value="December">December</option>
-            </select>
-            <button className="btn cal-btn" type="button">
-              <svg className="bi" width="16" height="16"></svg>
-            </button>
-          </div>
-          <div className="cal-weekdays text-body-secondary">
-            <div className="cal-weekday">Sun</div>
-            <div className="cal-weekday">Mon</div>
-            <div className="cal-weekday">Tue</div>
-            <div className="cal-weekday">Wed</div>
-            <div className="cal-weekday">Thu</div>
-            <div className="cal-weekday">Fri</div>
-            <div className="cal-weekday">Sat</div>
-          </div>
-          <div className="cal-days">
-            <button className="btn cal-btn" disabled="" type="button">
-              30
-            </button>
-            <button className="btn cal-btn" disabled="" type="button">
-              31
-            </button>
+    <div>
+      <b-calendar v-model="date" locale="en-US" />
+    </div>
+      <div className="d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-center">
+        <div
+          className="dropdown-menu d-block position-static p-2 mx-0 shadow rounded-3 w-340px"
+          data-bs-theme="light"
+        >
+          <div className="d-grid gap-1">
+            <div className="cal">
+              <div className="cal-month">
+                <button className="btn cal-btn" type="button">
+                  <svg className="bi" width="16" height="16">
+                    <use xlinkHref="#arrow-left-short"></use>
+                  </svg>
+                </button>
+                <strong className="cal-month-name">June</strong>
+                <select className="form-select cal-month-name d-none">
+                  <option value="January">January</option>
+                  <option value="February">February</option>
+                  <option value="March">March</option>
+                  <option value="April">April</option>
+                  <option value="May">May</option>
+                  <option selected="" value="June">
+                    June
+                  </option>
+                  <option value="July">July</option>
+                  <option value="August">August</option>
+                  <option value="September">September</option>
+                  <option value="October">October</option>
+                  <option value="November">November</option>
+                  <option value="December">December</option>
+                </select>
+                <button className="btn cal-btn" type="button">
+                  <svg className="bi" width="16" height="16">
+                    <use xlinkHref="#arrow-right-short"></use>
+                  </svg>
+                </button>
+              </div>
+              <div className="cal-weekdays text-body-secondary">
+                <div className="cal-weekday">Sun</div>
+                <div className="cal-weekday">Mon</div>
+                <div className="cal-weekday">Tue</div>
+                <div className="cal-weekday">Wed</div>
+                <div className="cal-weekday">Thu</div>
+                <div className="cal-weekday">Fri</div>
+                <div className="cal-weekday">Sat</div>
+              </div>
+              <div className="cal-days">
+                <button className="btn cal-btn" disabled="" type="button">
+                  30
+                </button>
+                <button className="btn cal-btn" disabled="" type="button">
+                  31
+                </button>
 
-            <button className="btn cal-btn" type="button">
-              1
-            </button>
-            <button className="btn cal-btn" type="button">
-              2
-            </button>
-            <button className="btn cal-btn" type="button">
-              3
-            </button>
-            <button className="btn cal-btn" type="button">
-              4
-            </button>
-            <button className="btn cal-btn" type="button">
-              5
-            </button>
-            <button className="btn cal-btn" type="button">
-              6
-            </button>
-            <button className="btn cal-btn" type="button">
-              7
-            </button>
+                <button className="btn cal-btn" type="button">
+                  1
+                </button>
+                <button className="btn cal-btn" type="button">
+                  2
+                </button>
+                <button className="btn cal-btn" type="button">
+                  3
+                </button>
+                <button className="btn cal-btn" type="button">
+                  4
+                </button>
+                <button className="btn cal-btn" type="button">
+                  5
+                </button>
+                <button className="btn cal-btn" type="button">
+                  6
+                </button>
+                <button className="btn cal-btn" type="button">
+                  7
+                </button>
 
-            <button className="btn cal-btn" type="button">
-              8
-            </button>
-            <button className="btn cal-btn" type="button">
-              9
-            </button>
-            <button className="btn cal-btn" type="button">
-              10
-            </button>
-            <button className="btn cal-btn" type="button">
-              11
-            </button>
-            <button className="btn cal-btn" type="button">
-              12
-            </button>
-            <button className="btn cal-btn" type="button">
-              13
-            </button>
-            <button className="btn cal-btn" type="button">
-              14
-            </button>
+                <button className="btn cal-btn" type="button">
+                  8
+                </button>
+                <button className="btn cal-btn" type="button">
+                  9
+                </button>
+                <button className="btn cal-btn" type="button">
+                  10
+                </button>
+                <button className="btn cal-btn" type="button">
+                  11
+                </button>
+                <button className="btn cal-btn" type="button">
+                  12
+                </button>
+                <button className="btn cal-btn" type="button">
+                  13
+                </button>
+                <button className="btn cal-btn" type="button">
+                  14
+                </button>
 
-            <button className="btn cal-btn" type="button">
-              15
-            </button>
-            <button className="btn cal-btn" type="button">
-              16
-            </button>
-            <button className="btn cal-btn" type="button">
-              17
-            </button>
-            <button className="btn cal-btn" type="button">
-              18
-            </button>
-            <button className="btn cal-btn" type="button">
-              19
-            </button>
-            <button className="btn cal-btn" type="button">
-              20
-            </button>
-            <button className="btn cal-btn" type="button">
-              21
-            </button>
+                <button className="btn cal-btn" type="button">
+                  15
+                </button>
+                <button className="btn cal-btn" type="button">
+                  16
+                </button>
+                <button className="btn cal-btn" type="button">
+                  17
+                </button>
+                <button className="btn cal-btn" type="button">
+                  18
+                </button>
+                <button className="btn cal-btn" type="button">
+                  19
+                </button>
+                <button className="btn cal-btn" type="button">
+                  20
+                </button>
+                <button className="btn cal-btn" type="button">
+                  21
+                </button>
 
-            <button className="btn cal-btn" type="button">
-              22
-            </button>
-            <button className="btn cal-btn" type="button">
-              23
-            </button>
-            <button className="btn cal-btn" type="button">
-              24
-            </button>
-            <button className="btn cal-btn" type="button">
-              25
-            </button>
-            <button className="btn cal-btn" type="button">
-              26
-            </button>
-            <button className="btn cal-btn" type="button">
-              27
-            </button>
-            <button className="btn cal-btn" type="button">
-              28
-            </button>
+                <button className="btn cal-btn" type="button">
+                  22
+                </button>
+                <button className="btn cal-btn" type="button">
+                  23
+                </button>
+                <button className="btn cal-btn" type="button">
+                  24
+                </button>
+                <button className="btn cal-btn" type="button">
+                  25
+                </button>
+                <button className="btn cal-btn" type="button">
+                  26
+                </button>
+                <button className="btn cal-btn" type="button">
+                  27
+                </button>
+                <button className="btn cal-btn" type="button">
+                  28
+                </button>
 
-            <button className="btn cal-btn" type="button">
-              29
-            </button>
-            <button className="btn cal-btn" type="button">
-              30
-            </button>
-            <button className="btn cal-btn" type="button">
-              31
-            </button>
+                <button className="btn cal-btn" type="button">
+                  29
+                </button>
+                <button className="btn cal-btn" type="button">
+                  30
+                </button>
+                <button className="btn cal-btn" type="button">
+                  31
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- client/src/components/MenuBuilder.jsx
+++ client/src/components/MenuBuilder.jsx
@@ -1,17 +1,461 @@
-//<use xlink:href="#arrow-left-short"></use>
-//<use xlink:href="#arrow-right-short"></use>
-
-import Calendar from "./Calendar";
-import Widget from "./Widget";
+import Calendar from './Calendar';
 
 export default function MenuBuilder() {
   return (
     <>
       <h2>Menu Builder</h2>
       <div>
-        {/* <Calendar /> */}
-        <Widget />
+        <Calendar />
       </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">
+              3 Items
+              <br />
+              Breakfast
+            </th>
+            <th scope="col">
+              2 Items
+              <br />
+              Snack
+            </th>
+            <th scope="col">
+              4 Items
+              <br />
+              Lunch
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">Monday</th>
+            <td className="align-middle">
+              <select className="selectpicker">
+                <optgroup label="Entree">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Drink">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+            </td>
+            <td>
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+            </td>
+            <td>
+              <select className="selectpicker">
+                <optgroup label="Entree">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Side">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Drink">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+            </td>
+            <td>
+              <button className="btn btn-primary" type="button">
+                Save
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">Monday</th>
+            <td className="align-middle">
+              <select className="selectpicker">
+                <optgroup label="Entree">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Drink">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+            </td>
+            <td>
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+            </td>
+            <td>
+              <select className="selectpicker">
+                <optgroup label="Entree">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Side">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Drink">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+            </td>
+            <td>
+              <button className="btn btn-primary" type="button">
+                Save
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">Monday</th>
+            <td className="align-middle">
+              <select className="selectpicker">
+                <optgroup label="Entree">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Drink">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+            </td>
+            <td>
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+            </td>
+            <td>
+              <select className="selectpicker">
+                <optgroup label="Entree">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Side">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Drink">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+            </td>
+            <td>
+              <button className="btn btn-primary" type="button">
+                Save
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">Monday</th>
+            <td className="align-middle">
+              <select className="selectpicker">
+                <optgroup label="Entree">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Drink">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+            </td>
+            <td>
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+            </td>
+            <td>
+              <select className="selectpicker">
+                <optgroup label="Entree">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Side">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Drink">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+            </td>
+            <td>
+              <button className="btn btn-primary" type="button">
+                Save
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">Monday</th>
+            <td className="align-middle">
+              <select className="selectpicker">
+                <optgroup label="Entree">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Drink">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+            </td>
+            <td>
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+            </td>
+            <td>
+              <select className="selectpicker">
+                <optgroup label="Entree">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Side">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Snack">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+              <select className="selectpicker">
+                <optgroup label="Drink">
+                  <option>Mustard</option>
+                  <option>Ketchup</option>
+                  <option>Relish</option>
+                </optgroup>
+              </select>
+              <br />
+            </td>
+            <td>
+              <button className="btn btn-primary" type="button">
+                Save
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 }
