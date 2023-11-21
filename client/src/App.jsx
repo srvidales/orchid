c76e7157905ec1472b285fea1b7d3351f8dbb9f4
charmingdarling <h1hasinai_f06@yahoo.com>
2023-11-20 21:06:41 -0800

    noticed that we were not using bootstrap css
    commented it out, because it would make our lives easier to have the library for bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Outlet, useLocation } from 'react-router-dom';
 import Header from './components/Header';
