da64b0bd968e56c7048ff705eef1ec34a2d4b807
Sergio Vidales Perez <srvidales@gmail.com>
2023-11-21 02:11:57 -0800

    WIP - Implementing MenuBuilder component

---
+++ client/src/components/MenuBuilder.jsx
@@ -0,0 +1,17 @@
+//<use xlink:href="#arrow-left-short"></use>
+//<use xlink:href="#arrow-right-short"></use>
+
+import Calendar from "./Calendar";
+import Widget from "./Widget";
+
+export default function MenuBuilder() {
+  return (
+    <>
+      <h2>Menu Builder</h2>
+      <div>
+        {/* <Calendar /> */}
+        <Widget />
+      </div>
+    </>
+  );
+}
