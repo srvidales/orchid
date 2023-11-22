d25c52a0533646d0fc4e996a04c8ecee7e40987d
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-21 18:38:25 -0800

    Auth.js - Made the comments more clear

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,41 +1,58 @@
-import decode from 'jwt-decode';
+import decode from 'jwt-decode'; // Import the `decode` function from the `jwt-decode` library
 
+// Define the `AuthService` class
 class AuthService {
+  // Method to get the profile information from the decoded token
   getProfile() {
+    // Call `decode` with the token to get the decoded payload
     return decode(this.getToken());
   }
 
+  // Method to check if the user is logged in
   loggedIn() {
+    // Get the token from local storage
     const token = this.getToken();
-    // If there is a token and it's not expired, return `true`
+    // Check if there is a token and it is not expired
+    // Return `true` if the token exists and is valid, otherwise return `false`
     return token && !this.isTokenExpired(token) ? true : false;
   }
 
+  // Method to check if the token has expired
   isTokenExpired(token) {
-    // Decode the token to get its expiration time that was set by the server
+    // Decode the token to get the expiration time
     const decoded = decode(token);
-    // If the expiration time is less than the current time (in seconds), the token is expired and we return `true`
+    // Compare the expiration time with the current time to check if the token is expired
     if (decoded.exp < Date.now() / 1000) {
+      // If the token is expired, remove it from local storage
       localStorage.removeItem('id_token');
+      // Return `true` to indicate that the token has expired
       return true;
     }
-    // If token hasn't passed its expiration time, return `false`
+    // Return `false` if the token has not expired yet
     return false;
   }
 
+  // Method to get the token from local storage
   getToken() {
     return localStorage.getItem('id_token');
   }
 
+  // Method to handle login
   login(idToken) {
+    // Store the token in local storage
     localStorage.setItem('id_token', idToken);
+    // Redirect the user to the root URL
     window.location.assign('/');
   }
 
+  // Method to handle logout
   logout() {
+    // Remove the token from local storage
     localStorage.removeItem('id_token');
+    // Reload the window to reset the application state
     window.location.reload();
   }
 }
 
+// Create an instance of the `AuthService` class and export it
 export default new AuthService();
