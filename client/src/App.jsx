6abbc7a38a91bc188ac20d6bac5cd9071b32af00
Kim Nguyen <124004580+charmingdarling@users.noreply.github.com>
2023-11-21 17:13:30 -0800

    Contact v2 (#31)
    * changed the textbox background to be light (white) instead of dark
    
    * apollo server v 3.5.0
    
    * did npm i and it updated
    
    * WIP. Contact Form - Checkboxes.
    
    * bootstrap and react-bootstrap installed in the client folder
    
    importing the bootstrap css file into main.jsx
    
    * Merged restored work for contact page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 // import './index.css';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 
 function App() {
-  // Moved this to it's own file in the component's folder
+   // Moved this to it's own file in the component's folder
   // const currentPage = useLocation().pathname;
   // console.log(currentPage);
 
