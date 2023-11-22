6c968add1db0f76ee7956008d7ca1512b9a8e6ab
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-21 16:35:38 -0800

    Navbar.jsx - Import the Auth to make the navbar conditional

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,27 +1,37 @@
-import { Button } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
-import "../index.css";
+import { Button } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import '../index.css';
+import Auth from '../utils/auth';
 
 const Navbar = () => {
   const links = [
-    { title: "Home", link: "/" },
-    { title: "About", link: "/about" },
-    { title: "Menu", link: "/menu" },
-    { title: "Parents", link: "/parents" },
-    { title: "Contact Us", link: "/contact" },
-    { title: "Login", link: "/login" },
+    { title: 'Home', link: '' },
+    { title: 'About', link: '/about' },
+    { title: 'Menu', link: '/menu' },
+    { title: 'Parents', link: '/parents' },
+    { title: 'Contact Us', link: '/contact' },
   ];
+
   return (
     <nav className="navbar">
-      {links.map((navlink) => (
-        <NavLink to={navlink.link}>
+      {links.map((navlink, i) => (
+        <NavLink to={navlink.link} key={i}>
           {({ isActive }) => (
-            <Button className={isActive ? "active" : ""}>
+            <Button className={isActive ? 'active' : ''}>
               {navlink.title}
             </Button>
           )}
         </NavLink>
       ))}
+      {!Auth.loggedIn() ? (
+        <NavLink to="/login">
+          {({ isActive }) => (
+            <Button className={isActive ? 'active' : ''}>Login/Signup</Button>
+          )}
+        </NavLink>
+      ) : (
+        <Button onClick={() => Auth.logout()}>LOGOUT</Button>
+      )}
     </nav>
   );
 };
