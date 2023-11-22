7791babb96101d52d15fde0e60e72265ad0580d8
charmingdarling <h1hasinai_f06@yahoo.com>
2023-11-22 12:54:36 -0800

    created a modal that is centered on screen horizontally and vertically

---
+++ client/src/components/VerticalModal.jsx
@@ -0,0 +1,28 @@
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {props.title}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>{props.p1}</p>
+        <p>{props.p2}</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default MyVerticallyCenteredModal;
