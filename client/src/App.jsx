997275e0875f43895b16d4bb4c7a0337ba5e93bb
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-21 16:34:38 -0800

    App.jsx - Copy the code for Apollo Provider from the Bootcamp Mern Modu le Act. 26

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,40 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 // import './index.css';
-import { Outlet, useLocation } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+
+import { setContext } from '@apollo/client/link/context';
+import { Outlet} from 'react-router-dom';
 import Header from './components/Header';
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   // Moved this to it's own file in the component's folder
@@ -13,13 +42,12 @@ function App() {
   // console.log(currentPage);
 
   return (
-    <>
+    <ApolloProvider client={client}>
       <div>
         <Header />
-        {/* <Navbar page={currentPage} /> */}
         <Outlet />
       </div>
-    </>
+    </ApolloProvider>
   );
 }
 
