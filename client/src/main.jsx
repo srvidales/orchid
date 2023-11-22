a685ecb00b8e226187aafe24467bfda6549ca9ec
charmingdarling <h1hasinai_f06@yahoo.com>
2023-11-21 23:16:42 -0800

    trying to figure out why their is no location to the '/' path

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,76 @@
+// import {
+//   createBrowserRouter,
+//   BrowserRouter as Router,
+//   Route,
+//   Routes,
+//   RouterProvider,
+// } from 'react-router-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App.jsx';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './index.css';
+// import Home from './pages/Home.jsx';
+// import About from './pages/About.jsx';
+// import Contact from './pages/Contact.jsx';
+// import Menu from './pages/Menu.jsx';
+// import Parents from './pages/Parents.jsx';
+// import Login from './pages/Login.jsx';
+// import Signup from './pages/Signup.jsx';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     errorElement: <h1>Error</h1>, // temporary placeholder until have error decided (404)
+//     children: [
+//       // array of children to this path
+//       {
+//         index: true, // saying we are still at the BASE route, which is '/'
+//         element: <Home />,
+//       },
+//       {
+//         path: '/about',
+//         element: <About />,
+//       },
+//       {
+//         path: '/menu',
+//         element: <Menu />,
+//       },
+//       {
+//         path: '/parents',
+//         element: <Parents />,
+//       },
+//       {
+//         path: '/contact',
+//         element: <Contact />,
+//       },
+//       {
+//         path: '/login',
+//         element: <Login />,
+//       },
+//       {
+//         path: '/signup',
+//         element: <Signup />,
+//       },
+//     ],
+//   },
+// ]);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//     <Router>
+//       <Routes>
+//         {/*Define nested routes here?*/}
+//         {/* <Route path="/" element={<App />} /> */}
+//       </Routes>
+//     </Router>
+//   </React.StrictMode>,
+// );
+
+// ! -----------------------------------------------
+
 import {
   createBrowserRouter,
   BrowserRouter as Router,
@@ -15,8 +88,8 @@ import About from './pages/About.jsx';
 import Contact from './pages/Contact.jsx';
 import Menu from './pages/Menu.jsx';
 import Parents from './pages/Parents.jsx';
-import Login from './pages/Login.jsx'
-import Signup from "./pages/Signup.jsx";
+import Login from './pages/Login.jsx';
+import Signup from './pages/Signup.jsx';
 
 const router = createBrowserRouter([
   {
@@ -50,7 +123,7 @@ const router = createBrowserRouter([
         element: <Login />,
       },
       {
-        path: "/signup",
+        path: '/signup',
         element: <Signup />,
       },
     ],
@@ -63,8 +136,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Router>
       <Routes>
         {/*Define nested routes here?*/}
+        {/* <Route path="/" element={<Home />} /> */}
         {/* <Route path="/" element={<App />} /> */}
       </Routes>
     </Router>
   </React.StrictMode>,
 );
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>,
+// );
