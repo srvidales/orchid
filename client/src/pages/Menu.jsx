7148091a09f5a650639dbb0bc9a73f493d6602be
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-22 19:32:41 -0800

    Menu - render MenuBuilder conditionally using the ternary operator

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -1,10 +1,16 @@
-import MenuBuilder from "../components/MenuBuilder";
+import MenuBuilder from '../components/MenuBuilder';
+import MenuView from '../components/MenuView'; // Import the MenuView
+import Auth from '../utils/auth'; // Import the Auth utility
 
 const Menu = () => {
+  // Check if the user is logged in
+  const isLoggedIn = Auth.loggedIn();
+
   return (
     <div>
       <h1>Menu</h1>
-      <MenuBuilder />
+      {/* Conditionally render MenuBuilder if the user is logged in, otherwise render MenuView */}
+      {isLoggedIn ? <MenuBuilder /> : <MenuView />}
     </div>
   );
 };
