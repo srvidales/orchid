72f4c7129719bff987654a4afd7dd3f7d7d0a2bd
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-25 00:19:53 -0800

    Menu - removed the const

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -3,9 +3,6 @@ import MenuView from "../components/MenuView";
 import Auth from '../utils/auth';
 
 const Menu = () => {
-  // Check if the user is logged in
-  const isLoggedIn = Auth.loggedIn();
-
   return (
     <div>
       {Auth.loggedIn() ? <MenuBuilder /> : <MenuView />}
