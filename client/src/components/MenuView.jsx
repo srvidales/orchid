89809c8f26751985acd2550919cb927eb77aafe1
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-26 15:48:11 -0800

    MenuView - Can only render snack because the categories match

--- client/src/components/MenuView.jsx
+++ client/src/components/MenuView.jsx
@@ -1,124 +1,122 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
-import { GET_DAILY_MENUS_BY_SCHOOL_AND_DATE } from '../utils/queries';
+import { GET_DAILY_MENUS_BY_SCHOOL } from '../utils/queries';
 
 export default function MenuView() {
   // State variables
   const [currentWeek, setCurrentWeek] = useState('');
   const [weekdays, setWeekdays] = useState([]);
   const [dates, setDates] = useState([]);
-  const [meals, setMeals] = useState([]);
+  const [menuData, setMenuData] = useState([]);
 
-  // GraphQL query to get daily menus by school and date
-  const { loading, data } = useQuery(GET_DAILY_MENUS_BY_SCHOOL_AND_DATE, {
-    variables: {
-      schoolId: '656308b295c8e42fb3501c94',
-      date: '11/20/23',
-    },
+  // GraphQL hook to fetch data
+  const { data, loading, error } = useQuery(GET_DAILY_MENUS_BY_SCHOOL, {
+    variables: { schoolId: '6563aef4c239cd4e485649e2' },
   });
 
-  // Update the week-related and meal information when the data changes
+  console.log('data from query', data);
+
+  // Update the week-related and menu item information when the data changes
   useEffect(() => {
-    // Get the current date
-    const currentDate = new Date();
+    const fetchData = async () => {
+      // Check if data is available
+      if (data && data.dailyMenusBySchool) {
+        // Extract daily menus from the fetched data
+        const dailyMenus = data.dailyMenusBySchool;
 
-    // Find the start date of the current week (Monday)
-    const startDate = new Date(currentDate);
-    startDate.setDate(
-      currentDate.getDate() -
-        currentDate.getDay() +
-        (currentDate.getDay() === 0 ? -6 : 1),
-    );
+        // Calculate the current week
+        const currentDate = new Date();
+        const startOfWeek = new Date(currentDate);
+        startOfWeek.setDate(
+          currentDate.getDate() -
+            currentDate.getDay() +
+            (currentDate.getDay() === 0 ? 1 : -6),
+        );
 
-    // Find the end date of the current week (Friday)
-    const endDate = new Date(startDate);
-    endDate.setDate(startDate.getDate() + 4);
+        const endOfWeek = new Date(startOfWeek);
+        endOfWeek.setDate(startOfWeek.getDate() + 4);
 
-    // Format the dates as "MM/DD/YY"
-    const formattedStartDate = `${
-      startDate.getMonth() + 1
-    }/${startDate.getDate()}/${startDate.getFullYear() % 100}`;
+        const formattedStartDate = `${
+          startOfWeek.getMonth() + 1
+        }/${startOfWeek.getDate()}/${startOfWeek.getFullYear() % 100}`;
+        const formattedEndDate = `${
+          endOfWeek.getMonth() + 1
+        }/${endOfWeek.getDate()}/${endOfWeek.getFullYear() % 100}`;
 
-    const formattedEndDate = `${endDate.getMonth() + 1}/${endDate.getDate()}/${
-      endDate.getFullYear() % 100
-    }`;
+        setCurrentWeek(`${formattedStartDate} - ${formattedEndDate}`);
 
-    // Set the current week string
-    setCurrentWeek(`${formattedStartDate} - ${formattedEndDate}`);
+        // Create an array of formatted day names for each weekday (Monday to Friday)
+        const formattedDays = [];
+        // Create an array of formatted dates for each weekday (Monday to Friday)
+        const formattedDates = [];
 
-    // Create an array of formatted day names for each weekday (Monday to Friday)
-    const formattedDays = [];
-    // Create an array of formatted dates for each weekday (Monday to Friday)
-    const formattedDates = [];
+        // Loop through each day of the week (Monday to Friday)
+        for (let i = 0; i < 5; i++) {
+          // Create a new Date object starting from the provided Monday
+          const date = new Date(startOfWeek);
+          // Increment the date to represent each day of the week
+          date.setDate(startOfWeek.getDate() + i);
 
-    // Loop through each day of the week (Monday to Friday)
-    for (let i = 0; i < 5; i++) {
-      // Create a new Date object starting from the provided Monday
-      const date = new Date(startDate);
-      // Increment the date to represent each day of the week
-      date.setDate(startDate.getDate() + i);
+          // Get the full day name using the 'en-US' locale
+          const dayName = new Intl.DateTimeFormat('en-US', {
+            weekday: 'long',
+          }).format(date);
 
-      // Get the full day name using the 'en-US' locale
-      const dayName = new Intl.DateTimeFormat('en-US', {
-        weekday: 'long',
-      }).format(date);
+          // Format the date as MM/DD/YY
+          const formattedDate = `${date.getMonth() + 1}/${date.getDate()}/${
+            date.getFullYear() % 100
+          }`;
 
-      // Format the date as MM/DD/YY
-      const formattedDate = `${date.getMonth() + 1}/${date.getDate()}/${
-        date.getFullYear() % 100
-      }`;
+          // Push the day name and formatted date to their respective arrays
+          formattedDays.push(dayName);
+          formattedDates.push(formattedDate);
+        }
 
-      // Push the day name and formatted date to their respective arrays
-      formattedDays.push(dayName);
-      formattedDates.push(formattedDate);
-    }
+        setWeekdays(formattedDays);
+        setDates(formattedDates);
 
-    setWeekdays(formattedDays);
-    setDates(formattedDates);
+        // Create an array to hold the menu data for each day
+        const menuDataByDay = [];
 
-    // Check if data is loaded
-    if (
-      !loading &&
-      data &&
-      data.dailyMenusBySchoolAndDate &&
-      data.dailyMenusBySchoolAndDate.length > 0
-    ) {
-      // Query returns only 1 meal
-      const firstMeal = data.dailyMenusBySchoolAndDate[0];
-      if (firstMeal) {
-        // Set up the meal based on the result
-        setMeals(firstMeal.meal);
+        // Loop through each day of the week (Monday to Friday)
+        for (let i = 0; i < 5; i++) {
+          // Find the corresponding DailyMenu for the current date
+          const dailyMenuForDay = dailyMenus.find(
+            (menu) =>
+              new Date(menu.date).toDateString() ===
+              new Date(formattedDates[i]).toDateString(),
+          );
+
+          // Check if a DailyMenu was found for the current date
+          if (dailyMenuForDay) {
+            // Extract menu items for the current day
+            const menuItems = dailyMenuForDay.menuItems.map((menuItem) => ({
+              name: menuItem.name,
+              description: menuItem.description,
+              category: menuItem.category,
+            }));
+
+            // Push the date and menu items to the menuDataByDay array
+            menuDataByDay.push({
+              date: formattedDates[i],
+              items: menuItems,
+            });
+          } else {
+            // If no DailyMenu was found for the current date, push an empty entry
+            menuDataByDay.push({
+              date: formattedDates[i],
+              items: [],
+            });
+          }
+        }
+        console.log('menuDataByDay:', menuDataByDay);
+        // Set the menuData state variable with the menuDataByDay array
+        setMenuData(menuDataByDay);
       }
-    }
-  }, [data]); // Run this effect when data changes
+    };
 
-  // Render menu items for a specific meal
-  const renderMenuItems = (meal) => {
-    // Check if data is still loading
-    if (loading) return <p>Loading...</p>;
-
-    // Check if there's an error in fetching data
-    if (!data || !data.dailyMenusBySchoolAndDate) {
-      return <p>Error fetching data</p>;
-    }
-
-    // Extract menu items for the specified meal
-    const mealItems = data.dailyMenusBySchoolAndDate[0]?.menuItems.filter(
-      (item) => item.category === meal,
-    );
-
-    // Check if there are no menu items for the specified meal
-    if (!mealItems || mealItems.length === 0) {
-      return <p>Bring your own.</p>;
-    }
-
-    // Render menu items
-    return mealItems.map((item) => (
-      <tr key={item._id}>
-        <td>{item.name}</td>
-      </tr>
-    ));
-  };
+    fetchData();
+  }, [data, loading]);
 
   // Return JSX
   return (
@@ -133,39 +131,38 @@ export default function MenuView() {
               <th key={index}>{day}</th>
             ))}
           </tr>
-          <tr>
-            {/* Render dates */}
-            {dates.map((date, index) => (
-              <td key={index}>{date}</td>
-            ))}
-          </tr>
         </thead>
         <tbody>
-          {/* Row for meal items */}
-          <tr className="meal-row">
-            <td colSpan="5">
-              <h3>Breakfast</h3>
-              <table>
-                <tbody>{renderMenuItems('BREAKFAST')}</tbody>
-              </table>
-            </td>
-          </tr>
-          <tr className="meal-row">
-            <td colSpan="5">
-              <h3>Lunch</h3>
-              <table>
-                <tbody>{renderMenuItems('LUNCH')}</tbody>
-              </table>
-            </td>
-          </tr>
-          <tr className="meal-row">
-            <td colSpan="5">
-              <h3>Snack</h3>
-              <table>
-                <tbody>{renderMenuItems('SNACK')}</tbody>
-              </table>
-            </td>
-          </tr>
+          {/* Render menu items for each day */}
+          {menuData.map((dayData, dayIndex) => (
+            <React.Fragment key={dayIndex}>
+              <tr>
+                <td colSpan={5} style={{ textAlign: 'center' }}>
+                  {dayData.date}
+                </td>
+              </tr>
+              {['BREAKFAST', 'LUNCH', 'SNACK'].map(
+                (category, categoryIndex) => (
+                  <React.Fragment key={categoryIndex}>
+                    <tr>
+                      <td colSpan={5} style={{ textAlign: 'center' }}>
+                        <strong>{category}</strong>
+                      </td>
+                    </tr>
+                    {dayData.items
+                      .filter((item) => item.category === category)
+                      .map((item, itemIndex) => (
+                        <tr key={itemIndex}>
+                          <td colSpan={5} style={{ textAlign: 'center' }}>
+                            {item.name}
+                          </td>
+                        </tr>
+                      ))}
+                  </React.Fragment>
+                ),
+              )}
+            </React.Fragment>
+          ))}
         </tbody>
       </table>
     </>
