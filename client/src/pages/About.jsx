26a8c955e62d1411e642330dfe8693b07c0ad444
Kim Nguyen <124004580+charmingdarling@users.noreply.github.com>
2023-11-26 13:46:21 -0800

    imported and added images into the cards (#52)
    reworded the description content so that it was more uniform

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,40 +3,41 @@ import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
+import OurDaycare from '../assets/images/3D/bus-trip.png';
+import Snacking from '../assets/images/3D/kids-snacking.png';
+import Matching from '../assets/images/3D/matching.png';
+import Sharing from '../assets/images/3D/sharing-oranges.png';
+
 function About() {
   const cardData = [
     {
       title: 'Our Daycare',
-      text: 'We are committed to upholding excellence and integrity in every facet of our daycare. Seeking inventive approaches to benefit our little learners is a priority. Collaboration and teamwork are essential components of our success. We extend dignity and respect to everyone we encounter. Our unwavering dedication is directed towards the well-being of families.',
+      text: 'Committed to upholding excellence and integrity, our daycare prioritizes inventive approaches to benefit little learners. Collaboration and teamwork are essential for our success. We extend dignity and respect to everyone we encounter, dedicating ourselves to the well-being of families.',
+      image: OurDaycare,
+    },
+    {
+      title: 'Values',
+      text: "Our learning approach and curriculum encourages exploration and progress tailored to each child's unique pace and style. We foster social, physical, emotional, and intellectual growth. Our team of educators are committed to guiding, nurturing, and cultivating the full spectrum of your child's development.",
+      image: Sharing,
     },
     {
       title: 'Vision',
-      text: 'We take pride in empowering and celebrating each milestone of their development. Our facility bridges a new space from home to school to help your little ones become individual thinkers and active participants in their community.',
+      text: "We take pride in ensuring that Wicked Whippersnappers focuses on providing healthy and well-balanced meals, meeting your child's nutritional needs, and nurturing their overall well-being.",
+      image: Matching,
     },
     {
       title: 'Mission',
       text: "Through our dedication in focusing on providing healthy and well-balanced meals, Wicked Whippersnappers ensures that your child's nutritional needs are met, nurturing their well-being.",
-    },
-    {
-      title: 'Values',
-      text: "Our learning approach and curriculum encourages exploration and progress tailored to each child's unique pace and style. We foster social, physical, emotional, and intellectual growth. Our team of highly trained educators is passionately committed to guiding, nurturing, and cultivating the full spectrum of your child's development.",
+      image: Snacking,
     },
   ];
 
   return (
     <Row md={2} className="g-3" style={{ display: 'flex', flexWrap: 'wrap' }}>
       {cardData.map((card, idx) => (
-        <Col
-          key={idx}
-          xs={12}
-          sm={6}
-          md={6}
-          lg={6}
-          xl={6}
-          // style={{ flex: '1', margin: '0' }}
-        >
+        <Col key={idx} xs={12} sm={6} md={6} lg={6} xl={6}>
           <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Img variant="top" src={card.image} />
             <Card.Body>
               <Card.Title>{card.title}</Card.Title>
               <Card.Text>{card.text}</Card.Text>
