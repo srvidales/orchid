9846ce4eead7097994a2c39c994b8d48c5909f55
charmingdarling <h1hasinai_f06@yahoo.com>
2023-11-26 21:04:48 -0800

    imported AddItem2 component

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -1,11 +1,14 @@
-import MenuBuilder from "../components/MenuBuilder";
-import MenuView from "../components/MenuView";
+import MenuBuilder from '../components/MenuBuilder';
+import MenuView from '../components/MenuView';
 import Auth from '../utils/auth';
+
+import AddItem2 from '../components/AddItem2';
 
 const Menu = () => {
   return (
     <div>
       {Auth.loggedIn() ? <MenuBuilder /> : <MenuView />}
+      <AddItem2 />
     </div>
   );
 };
