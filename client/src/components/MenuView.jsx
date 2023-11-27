9d9e4c6723f80217939afed05b0a183ea55973d0
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-27 00:02:56 -0800

    Menu view (#58)
    * Menu - render MenuBuilder conditionally using the ternary operator
    
    * Resolvers - Add a new query that will find daily menus for a specific date
    
    * Models - Remove getters for dates
    
    * Nav - style should be consistent now. App.css - switch the max-width from 1280px to 100%
    
    * Nav - remove Container
    
    * Nav - Add a logo image
    
    * Resolvers - Add comments to dailyMenusBySchoolAndDate and dailyMenusBySchoolDateAndMeal
    
    * Queries - Added to the queries file.
    
    * Menu - removed the const
    
    * Nav - remove Parents Tab
    
    * MenuView - Render the current week under the MenuView header
    
    * MenuView - Dynamically render that dates and the days
    
    * Switched the AuthenticationError into generic Error'
    
    * Resolvers - Add to the dailyMenusBySchoolDateAndMeal to populate the items inside the associated school
    
    * Queries - edit the GET_DAILY_MENUS_BY_SCHOOL_DATE_AND_MEAL
    
    * Resolvers - dailyMenusBySchoolAndDate populate school
    
    * Seeds - more daycare friendly
    
    * MenuView - Having trouble rendering the menu items
    
    * Added a new resolver to get the daily menu
    
    * DailyMenuSeeds - added more dates because the previous one only had 1 week
    
    * Resolvers - sorting the dailyMenusBySchool
    
    * MenuView - Can only render snack because the categories match
    
    * MenuView - Able to render items pulled from the database
    
    * Seed - fixing the seeds
    
    * Seeds - git merge with main
    
    * Resolvers - dailyMenuBySchool
    
    * Seed - revert back
    
    * MenuView - render the browser but need to change the query being used

--- client/src/components/MenuView.jsx
+++ client/src/components/MenuView.jsx
@@ -1,8 +1,52 @@
-// COMPONENT IS FOR DEMO PURPOSES ONLY //
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_DAILY_MENUS } from '../utils/queries';
+
+// Component to display daily menus
 export default function MenuView() {
-    return (
-      <>
-        <h2>Menu View</h2>
-      </>
-    );
-  }
+  // GraphQL hook to fetch data
+  const { data, loading, error } = useQuery(GET_DAILY_MENUS);
+
+  // Extract daily menu data or default to an empty array
+  const dailyMenuData = data?.dailyMenus || [];
+
+  // Group daily menu data by date using a custom utility function
+  const result = Object.groupBy(dailyMenuData, ({ date }) => date);
+  console.log('data from query', result);
+
+  // Get an array of keys (dates) from the grouped result
+  const objectKeyArry = Object.keys(result);
+  console.log(result);
+
+  // Return JSX
+  return (
+    <>
+      <h1>Menu View</h1>
+      <div className="d-flex flex-wrap justify-content-center">
+        {/* Iterate over each date in the grouped result */}
+        {objectKeyArry.map((item) => (
+          <div style={{ width: '18%' }} className="border " key={item}>
+            <div>
+              {/* Display the date as a heading */}
+              <h2>{item}</h2>
+            </div>
+            {/* Use div for the body instead of tbody */}
+            <div>
+              {/* Iterate over each day's data for the current date */}
+              {result[item].map((dayData, index) => (
+                <div key={index}>
+                  <div style={{ textAlign: 'center' }}>
+                    {/* Display the meal type as a sub-heading */}
+                    <h3>{dayData.meal}</h3>
+                    {/* Display the first menu item for the current meal */}
+                    <p>{dayData.menuItems[0].name}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
