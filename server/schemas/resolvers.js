6d3eccba4d91fb17788be7a321520d47d82132b8
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-26 21:00:46 -0800

    Extra functionalities (#56)
    * User - update and delete
    
    * Resolvers - add, update and delete schools
    
    * Resolvers - update and delete daily menu'
    
    * Resolvers - add, update and delete menu items

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,11 +2,9 @@
 const { User, MenuItem, School, DailyMenu } = require('../models');
 const bcrypt = require('bcrypt');
 const { generateToken, loginUser } = require('../utils/auth');
-const { AuthenticationError } = require('@apollo/server');
 
 // Creating GraphQL resolvers
 const resolvers = {
-
   Query: {
     // Resolver for fetching all users
     users: async () => {
@@ -16,9 +14,7 @@ const resolvers = {
       } catch (error) {
         // Log and throw any errors that occur during the query
         console.error('Error during user fetch:', error);
-        throw new AuthenticationError(
-          'An error occurred while fetching users.',
-        );
+        throw new Error('An error occurred while fetching users.');
       }
     },
 
@@ -33,9 +29,7 @@ const resolvers = {
         return dailyMenus;
       } catch (error) {
         console.error('Error during daily menu fetch:', error);
-        throw new AuthenticationError(
-          'An error occurred while fetching daily menu.',
-        );
+        throw new Error('An error occurred while fetching daily menu.');
       }
     },
 
@@ -54,9 +48,7 @@ const resolvers = {
         return dailyMenus;
       } catch (error) {
         console.error('Error during daily menu fetch:', error);
-        throw new AuthenticationError(
-          'An error occurred while fetching daily menu.',
-        );
+        throw new Error('An error occurred while fetching daily menu.');
       }
     },
 
@@ -100,9 +92,7 @@ const resolvers = {
       } catch (error) {
         // Log and throw any errors that occur during the query
         console.error(`Error during school fetch for ID ${_id}:`, error);
-        throw new AuthenticationError(
-          'An error occurred while fetching the school.',
-        );
+        throw new Error('An error occurred while fetching the school.');
       }
     },
 
@@ -115,9 +105,7 @@ const resolvers = {
       } catch (error) {
         // Log and throw any errors that occur during the query
         console.error('Error during menu items fetch:', error);
-        throw new AuthenticationError(
-          'An error occurred while fetching menu items.',
-        );
+        throw new Error('An error occurred while fetching menu items.');
       }
     },
 
@@ -138,9 +126,7 @@ const resolvers = {
       } catch (error) {
         // Log and throw any errors that occur during the query
         console.error('Error during daily menus fetch:', error);
-        throw new AuthenticationError(
-          'An error occurred while fetching daily menus.',
-        );
+        throw new Error('An error occurred while fetching daily menus.');
       }
     },
   },
@@ -174,9 +160,7 @@ const resolvers = {
       } catch (error) {
         console.error('Error during user signup:', error);
         // Handle the error and throw it or return an error message
-        throw new AuthenticationError(
-          'An error occurred during signup. Please try again.',
-        );
+        throw new Error('An error occurred during signup. Please try again.');
       }
     },
 
@@ -189,7 +173,7 @@ const resolvers = {
         // Check if the login was successful
         if (!result || !result.token || !result.user) {
           // If not successful, throw an authentication error
-          throw new AuthenticationError('Invalid credentials');
+          throw new Error('Invalid credentials');
         }
 
         // If successful, return the token and user information
@@ -197,7 +181,7 @@ const resolvers = {
       } catch (error) {
         // Log and throw any errors that occur during login
         console.error(error);
-        throw new AuthenticationError('Invalid credentials');
+        throw new Error('Invalid credentials');
       }
     },
 
@@ -231,9 +215,109 @@ const resolvers = {
       } catch (error) {
         // Log and throw any errors that occur during updating user
         console.error(error);
-        throw new AuthenticationError(
+        throw new Error(
           'An error occurred during user update. Please try again.',
         );
+      }
+    },
+
+    // Mutation resolver for deleting a user
+    deleteUser: async (_parent, { userId, schoolId }) => {
+      try {
+        // Find the user by ID
+        const user = await User.findById(userId);
+
+        // Check if the user exists
+        if (!user) {
+          throw new Error('User not found.');
+        }
+
+        // Find the school by ID
+        const school = await School.findById(schoolId);
+
+        // Check if the school exists
+        if (!school) {
+          throw new Error('School not found.');
+        }
+
+        // Use $pull to remove the user from the school's user array
+        await School.updateOne({ _id: schoolId }, { $pull: { users: userId } });
+
+        // Delete the user from the database
+        await User.deleteOne({ _id: userId });
+
+        // Return a success message or updated school
+        return 'User deleted from school successfully.';
+      } catch (error) {
+        // Log and throw any errors that occur during user deletion
+        console.error(error);
+        throw new Error('An error occurred during user deletion.');
+      }
+    },
+
+    // Mutation resolver for adding a school
+    addSchool: async (_parent, schoolInfo) => {
+      try {
+        // Create a new school in the database
+        const newSchool = await School.create(schoolInfo);
+
+        // Return the newly created school
+        return newSchool;
+      } catch (error) {
+        // Log and throw any errors that occur during school creation
+        console.error(error);
+        throw new Error('An error occurred during school creation.');
+      }
+    },
+
+    // Mutation resolver for updating a school
+    updateSchool: async (_parent, { schoolId, ...updatedFields }) => {
+      try {
+        // Find the school by ID and update its fields
+        const updatedSchool = await School.findByIdAndUpdate(
+          schoolId,
+          { $set: updatedFields },
+          { new: true },
+        );
+
+        // Return the updated school
+        return updatedSchool;
+      } catch (error) {
+        // Log and throw any errors that occur during school update
+        console.error(error);
+        throw new Error('An error occurred during school update.');
+      }
+    },
+
+    // Mutation resolver for deleting a school
+    deleteSchool: async (_parent, { schoolId }) => {
+      try {
+        // Find the school by ID
+        const school = await School.findById(schoolId);
+
+        // Check if the school exists
+        if (!school) {
+          throw new Error('School not found.');
+        }
+
+        // Get the user IDs associated with the school
+        const userIDs = school.users.map((user) => user.toString());
+
+        // Remove the school reference from all associated users
+        await User.updateMany(
+          { _id: { $in: userIDs } },
+          { $pull: { schools: schoolId } },
+        );
+
+        // Delete the school and all of its users from the database
+        await School.deleteOne({ _id: schoolId });
+
+        // Return a success message
+        return 'School and its users deleted successfully.';
+      } catch (error) {
+        // Log and throw any errors that occur during school deletion
+        console.error(error);
+        throw new Error('An error occurred during school deletion.');
       }
     },
 
@@ -257,9 +341,125 @@ const resolvers = {
       } catch (error) {
         // Log and throw any errors that occur during daily menu creation
         console.error(error);
-        throw new AuthenticationError(
+        throw new Error(
           'An error occurred during daily menu creation. Please try again.',
         );
+      }
+    },
+
+    // Mutation resolver for updating a daily menu
+    updateDailyMenu: async (
+      _parent,
+      { dailyMenuId, date, meal, menuItems },
+    ) => {
+      try {
+        // Find the daily menu by ID
+        const dailyMenu = await DailyMenu.findByIdAndUpdate(
+          dailyMenuId,
+          {
+            // Only update the fields that are provided
+            ...(date && { date }),
+            ...(meal && { meal }),
+            ...(menuItems && { menuItems }),
+          },
+          { new: true }, // Return the updated document after the update is applied
+        ).populate('menuItems');
+
+        // Return the updated daily menu
+        return dailyMenu;
+      } catch (error) {
+        console.error(error);
+        throw new Error('An error occurred during daily menu update.');
+      }
+    },
+
+    // Mutation resolver for deleting a daily menu
+    deleteDailyMenu: async (_parent, { dailyMenuId }) => {
+      try {
+        // Find the daily menu by ID
+        const dailyMenu = await DailyMenu.findById(dailyMenuId);
+
+        // Check if the daily menu exists
+        if (!dailyMenu) {
+          throw new Error('Daily Menu not found.');
+        }
+
+        // Delete the daily menu from the database
+        await DailyMenu.deleteOne({ _id: dailyMenuId });
+
+        // Return a success message
+        return 'Daily Menu deleted successfully.';
+      } catch (error) {
+        console.error(error);
+        throw new Error('An error occurred during Daily Menu deletion.');
+      }
+    },
+
+    // Mutation resolver for adding a menu item
+    addMenuItem: async (_parent, { name, description, image, category }) => {
+      try {
+        // Create a new menu item in the database
+        const menuItem = await MenuItem.create({
+          name,
+          description,
+          image,
+          category,
+        });
+
+        // Return the newly created menu item
+        return menuItem;
+      } catch (error) {
+        console.error(error);
+        throw new Error('An error occurred during menu item creation.');
+      }
+    },
+
+    // Mutation resolver for updating a menu item
+    updateMenuItem: async (
+      _parent,
+      { itemId, name, description, image, category },
+    ) => {
+      try {
+        // Find the menu item by ID and update its fields
+        const updatedMenuItem = await MenuItem.findByIdAndUpdate(
+          itemId,
+          {
+            // Only update the fields that are provided
+            ...(name && { name }),
+            ...(description && { description }),
+            ...(image && { image }),
+            ...(category && { category }),
+          },
+          { new: true }, // Return the updated document after the update is applied
+        );
+
+        // Return the updated menu item
+        return updatedMenuItem;
+      } catch (error) {
+        console.error(error);
+        throw new Error('An error occurred during menu item update.');
+      }
+    },
+
+    // Mutation resolver for deleting a menu item
+    deleteMenuItem: async (_parent, { itemId }) => {
+      try {
+        // Find the menu item by ID
+        const menuItem = await MenuItem.findById(itemId);
+
+        // Check if the menu item exists
+        if (!menuItem) {
+          throw new Error('Menu item not found.');
+        }
+
+        // Delete the menu item from the database
+        await MenuItem.deleteOne({ _id: itemId });
+
+        // Return a success message
+        return 'Menu item deleted successfully.';
+      } catch (error) {
+        console.error(error);
+        throw new AuthenticationError('An error occurred during menu item deletion.');
       }
     },
   },
