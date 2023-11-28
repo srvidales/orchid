4465bc55799d903530acc0cda53aa3a751bac4c5
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-27 21:28:13 -0800

    MenuView - Only show the weekdays

--- client/src/components/MenuView.jsx
+++ client/src/components/MenuView.jsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import { memo } from 'react';
 import { useQuery } from '@apollo/client';
-import { GET_DAILY_MENUS } from '../utils/queries';
+import { GET_DAILY_MENUS_BY_SCHOOL } from '../utils/queries';
 
 // Component to display daily menus
-export default function MenuView({ schoolId }) {
+export default memo(function MenuView({ schoolId }) {
   console.log('School ID Given:', schoolId);
   // GraphQL hook to fetch data
-  const { data, loading, error } = useQuery(GET_DAILY_MENUS);
+  const { data, error } = useQuery(GET_DAILY_MENUS_BY_SCHOOL, {
+    variables: { schoolId },
+  });
+
+  console.log('#2', data);
 
   // Extract daily menu data or default to an empty array
-  const dailyMenuData = data?.dailyMenus || [];
+  const dailyMenuData = data?.dailyMenusBySchool || [];
 
   // Group daily menu data by date using a custom utility function
-  const result = Object.groupBy(dailyMenuData, ({ date }) => date);
-  console.log('data from query', result);
+  const MenusByDate = Object.groupBy(dailyMenuData, ({ date }) => {
+    // Format the date using .toLocaleDateString()
+    const formattedDate = new Date(date).toLocaleDateString();
+    return formattedDate;
+  });
 
-  // Get an array of keys (dates) from the grouped result
-  const objectKeyArry = Object.keys(result);
-  console.log(result);
+  console.log('data from query', MenusByDate);
+
+  // Get an array of keys (dates) from the grouped MenusByDate, filtering out weekends
+  const filterWeekdays = (date) => {
+    const dayOfWeek = new Date(date).getDay();
+    return dayOfWeek >= 1 && dayOfWeek <= 5; // Monday (1) to Friday (5)
+  };
+
+  const weekdayKeysArray = Object.keys(MenusByDate).filter(filterWeekdays);
 
   // Create a function to format the current week in mm/dd/yy - mm/dd/yy
   const getCurrentWeek = () => {
@@ -32,28 +45,22 @@ export default function MenuView({ schoolId }) {
     return `${startOfWeek.toLocaleDateString()} - ${endOfWeek.toLocaleDateString()}`;
   };
 
-  // Return JSX
   return (
     <>
       <h1>Menu View</h1>
-      <h2>Current Week: {getCurrentWeek()}</h2>
+      <h2>Current Week:</h2>
+      <h3>{getCurrentWeek()}</h3>
       <div className="d-flex flex-wrap justify-content-center">
-        {/* Iterate over each date in the grouped result */}
-        {objectKeyArry.map((item) => (
-          <div style={{ width: '18%' }} className="border " key={item}>
+        {weekdayKeysArray.map((dateKey) => (
+          <div style={{ width: '18%' }} className="border" key={dateKey}>
             <div>
-              {/* Display the date as a heading */}
-              <h2>{item}</h2>
+              <h3>{dateKey}</h3>
             </div>
-            {/* Use div for the body instead of tbody */}
             <div>
-              {/* Iterate over each day's data for the current date */}
-              {result[item].map((dayData, index) => (
+              {MenusByDate[dateKey].map((dayData, index) => (
                 <div key={index}>
                   <div style={{ textAlign: 'center' }}>
-                    {/* Display the meal type as a sub-heading */}
-                    <h3>{dayData.meal}</h3>
-                    {/* Display the first menu item for the current meal */}
+                    <h4>{dayData.meal}</h4>
                     <p>{dayData.menuItems[0].name}</p>
                   </div>
                 </div>
@@ -64,4 +71,4 @@ export default function MenuView({ schoolId }) {
       </div>
     </>
   );
-}
+});
