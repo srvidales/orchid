b6e42f1ac03e27520fa251a2b70a446b240bcf71
charmingdarling <h1hasinai_f06@yahoo.com>
2023-11-27 19:28:11 -0800

    changed AddItem2 to Add

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -1,8 +1,7 @@
 import MenuBuilder from '../components/MenuBuilder';
 import MenuView from '../components/MenuView';
+import Add from '../components/Add';
 import Auth from '../utils/auth';
-
-import AddItem2 from '../components/AddItem2';
 
 const Menu = () => {
   const schoolId = '6563ed8cb2a2e8bdeee5cbc2';
@@ -10,7 +9,7 @@ const Menu = () => {
   return (
     <div>
       {Auth.loggedIn() ? <MenuBuilder /> : <MenuView />}
-      <AddItem2 />
+      <Add />
       {Auth.loggedIn() ? <MenuBuilder schoolId={schoolId} /> : <MenuView />}
     </div>
   );
